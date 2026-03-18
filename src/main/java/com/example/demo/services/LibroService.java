3cc6927dbbb171b52bdf1b22dcd67a8d0857bca1
Nicolas <nico.mv1998@gmail.com>
2026-03-18 10:07:36 -0300

    creando class LibroService y funciones varias

---
+++ src/main/java/com/example/demo/services/LibroService.java
@@ -0,0 +1,42 @@
+package com.example.demo.services;
+
+import com.example.demo.models.Libro;
+import com.example.demo.repositories.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+
+public class LibroService {
+    @Autowired
+    private LibroRepository libroRepository;
+
+
+    public List<Libro> getAll(){
+        return null;
+    }
+
+    public Libro getLibroById(long id){
+        return null;
+    }
+
+    public Libro getLibroByIsbn(String isbn){
+        return null;
+    }
+
+    public Libro save(Libro libroNew){
+        return null;
+
+    }
+
+    public Libro update(Libro libroUpdate){
+        return null;
+    }
+
+    public void delete(Long id){
+
+    }
+}
