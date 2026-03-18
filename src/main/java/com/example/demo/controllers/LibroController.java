abc9cd6d8a639400e6554cdca7bfadcee6d5b6a3
Nicolas <nico.mv1998@gmail.com>
2026-03-18 10:38:31 -0300

    agregando dependencia validation

--- src/main/java/com/example/demo/controllers/LibroController.java
+++ src/main/java/com/example/demo/controllers/LibroController.java
@@ -2,22 +2,31 @@ package com.example.demo.controllers;
 
 
 import com.example.demo.models.Libro;
+import com.example.demo.services.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/libros")
 public class LibroController {
 
+    @Autowired
+    private LibroService libroService;
+
     @GetMapping
     public ResponseEntity<List<Libro>> findAll(){
         List<Libro> libros = this.libroSerive.getAll();
         return ResponseEntity.status(HttpStatus.OK).body(libros);
+    }
+
+    @PostMapping
+    public ResponseEntity<Libro> Save(@RequestBody Libro libro){
+
     }
 
 
